6096ae7e85e36ef0b78dcf4aee8da306e66a00c6
BeliyBro33 <59355296+BeliyBro33@users.noreply.github.com>
2024-11-05 10:59:06 +0300

    Update humancheck.sh

--- humancheck.sh
+++ humancheck.sh
@@ -81,6 +81,8 @@ sudo rm -r  "/root/humancheck/humancheck.sh"
 echo "Done"
 curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": "'"$mchat"'", "text": "Обновление выполнено!" "disable_notification": false}' https://api.telegram.org/bot$token/sendMessage
 sleep 2
+echo "щас рестартну"
+sudo systemctl restart human
 }
 
 #функция для отправки ссылки на утентификацию
